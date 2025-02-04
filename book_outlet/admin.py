bee918ed5c5238450e6eb86d4f7d3be813e5fd64
Adithya <adithya11.v@gmail.com>
2025-02-04 09:43:26 +0530

    book
    	new file:   .vscode/settings.json
    	new file:   book_outlet/__init__.py
    	new file:   book_outlet/__pycache__/__init__.cpython-311.pyc
    	new file:   book_outlet/__pycache__/admin.cpython-311.pyc
    	new file:   book_outlet/__pycache__/apps.cpython-311.pyc
    	new file:   book_outlet/__pycache__/models.cpython-311.pyc
    	new file:   book_outlet/__pycache__/urls.cpython-311.pyc
    	new file:   book_outlet/__pycache__/views.cpython-311.pyc
    	new file:   book_outlet/admin.py
    	new file:   book_outlet/apps.py
    	new file:   book_outlet/migrations/0001_initial.py
    	new file:   book_outlet/migrations/0002_book_author_book_is_bestselling_alter_book_rating.py
    	new file:   book_outlet/migrations/0003_book_slug.py
    	new file:   book_outlet/migrations/0004_author_alter_book_slug_alter_book_author.py
    	new file:   book_outlet/migrations/0005_alter_book_author.py
    	new file:   book_outlet/migrations/__init__.py
    	new file:   book_outlet/migrations/__pycache__/0001_initial.cpython-311.pyc
    	new file:   book_outlet/migrations/__pycache__/0002_book_author_book_is_bestselling_alter_book_rating.cpython-311.pyc
    	new file:   book_outlet/migrations/__pycache__/0003_book_slug.cpython-311.pyc
    	new file:   book_outlet/migrations/__pycache__/0004_author_alter_book_slug_alter_book_author.cpython-311.pyc
    	new file:   book_outlet/migrations/__pycache__/0005_alter_book_author.cpython-311.pyc
    	new file:   book_outlet/migrations/__pycache__/__init__.cpython-311.pyc
    	new file:   book_outlet/models.py
    	new file:   book_outlet/templates/book_outlet/base.html
    	new file:   book_outlet/templates/book_outlet/book_detail.html
    	new file:   book_outlet/templates/book_outlet/index.html
    	new file:   book_outlet/tests.py
    	new file:   book_outlet/urls.py
    	new file:   book_outlet/views.py
    	new file:   book_store/__init__.py
    	new file:   book_store/__pycache__/__init__.cpython-311.pyc
    	new file:   book_store/__pycache__/settings.cpython-311.pyc
    	new file:   book_store/__pycache__/urls.cpython-311.pyc
    	new file:   book_store/__pycache__/wsgi.cpython-311.pyc
    	new file:   book_store/asgi.py
    	new file:   book_store/settings.py
    	new file:   book_store/urls.py
    	new file:   book_store/wsgi.py
    	new file:   db.sqlite3
    	new file:   manage.py

---
+++ book_outlet/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Book,Author
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug":("title",)}
+    list_display=("title","author",)
+    list_filter=("author","rating",)
+    
+admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
